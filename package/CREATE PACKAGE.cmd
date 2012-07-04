c6b6ef2af228765c3bd40aa427769c24d3f5a696
Duncan Smart <duncan.smart@gmail.com>
2012-07-04 18:36:40 +0100

    v2.1 for nuget

--- package/CREATE PACKAGE.cmd
+++ package/CREATE PACKAGE.cmd
@@ -2,11 +2,11 @@
 pushd "%~dp0"
 
 :: Get and increment BUILDVER from buildver.txt
-for /F %%V in (buildver.txt) do set /a BUILDVER=%%V + 1
+::for /F %%V in (buildver.txt) do set /a BUILDVER=%%V + 1
+::echo BUILDVER='%BUILDVER%'
+set /a BUILDVER=0
 
-echo BUILDVER='%BUILDVER%'
-
-nuget pack LessCoffee.nuspec -version 2.0.0.%BUILDVER%
+nuget pack LessCoffee.nuspec -version 2.1.0.%BUILDVER%
 if %ERRORLEVEL% == 0 (
     echo %BUILDVER% > buildver.txt
 )
