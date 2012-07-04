ab6ea8320bc6e08c60ec8ffe826f684072aaf651
Duncan Smart <duncan.smart@gmail.com>
2012-07-04 19:06:38 +0100

    Add output cache profile to web.config

--- package/CREATE PACKAGE.cmd
+++ package/CREATE PACKAGE.cmd
@@ -6,7 +6,7 @@ pushd "%~dp0"
 ::echo BUILDVER='%BUILDVER%'
 set /a BUILDVER=0
 
-nuget pack LessCoffee.nuspec -version 2.1.0.%BUILDVER%
+nuget pack LessCoffee.nuspec -version 2.1.1.%BUILDVER%
 if %ERRORLEVEL% == 0 (
     echo %BUILDVER% > buildver.txt
 )
