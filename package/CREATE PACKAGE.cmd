777002b7a2403dcc1d2ece57a68d0376426650d5
Duncan Smart <duncan.smart@gmail.com>
2012-09-15 03:02:06 +0100

    New nuget build for updated node.exe and coffeescript

--- package/CREATE PACKAGE.cmd
+++ package/CREATE PACKAGE.cmd
@@ -4,7 +4,7 @@ pushd "%~dp0"
 :: Get and increment BUILDVER from buildver.txt
 ::for /F %%V in (buildver.txt) do set /a BUILDVER=%%V + 1
 ::echo BUILDVER='%BUILDVER%'
-set /a BUILDVER=0
+set /a BUILDVER=1
 
 nuget pack LessCoffee.nuspec -version 2.2.0.%BUILDVER%
 if %ERRORLEVEL% == 0 (
