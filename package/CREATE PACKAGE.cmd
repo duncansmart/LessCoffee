26dff4ecfda7bb080c7b9d2871c670be29ff7caf
Duncan Smart <duncan.smart@gmail.com>
2013-07-17 16:52:30 +0100

    Added AjaxMin 4.* as nuget dependency (version 2.4.0.2)

--- package/CREATE PACKAGE.cmd
+++ package/CREATE PACKAGE.cmd
@@ -6,7 +6,7 @@ pushd "%~dp0"
 ::echo BUILDVER='%BUILDVER%'
 ::set /a BUILDVER=0
 
-nuget pack LessCoffee.nuspec -version 2.4.0.1
+nuget pack LessCoffee.nuspec -version 2.4.0.2
 ::if %ERRORLEVEL% == 0 (
 ::    echo %BUILDVER% > buildver.txt
 ::)
