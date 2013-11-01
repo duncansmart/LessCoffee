083c4fc2073c2dacd79ef133a0fa2e6d2c761c96
Duncan Smart <duncan.smart@gmail.com>
2013-11-01 18:58:47 +0000

    Do a release build before packaging.

--- package/CREATE PACKAGE.cmd
+++ package/CREATE PACKAGE.cmd
@@ -1,12 +1,8 @@
 @echo off
 pushd "%~dp0"
 
-:: Get and increment BUILDVER from buildver.txt
-::for /F %%V in (buildver.txt) do set /a BUILDVER=%%V + 1
-::echo BUILDVER='%BUILDVER%'
-::set /a BUILDVER=0
+echo Release build LessCoffee.csproj ...
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe ..\src\Handlers\LessCoffee.csproj /nologo /t:Build /p:Configuration=Release /verbosity:quiet
 
-nuget pack LessCoffee.nuspec -version 2.5.0.0
-::if %ERRORLEVEL% == 0 (
-::    echo %BUILDVER% > buildver.txt
-::)
+nuget pack LessCoffee.nuspec -version 2.5.0.0 -NoPackageAnalysis
+
