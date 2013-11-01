ae57775001bb4ace9eb7233259c8dd8933ef2290
Duncan Smart <duncan.smart@gmail.com>
2013-11-01 16:03:27 +0000

    pre-build.cmd downloads nodejs dependencies

---
+++ src/Handlers/pre-build.cmd
@@ -0,0 +1,55 @@
+@echo off
+
+if "%1"=="CLEAN" (
+	if exist "%~dp0\Resources\nodejs.tgz" del "%~dp0\Resources\nodejs.tgz"
+	goto :EOF
+)
+
+:: create httpget.js for use with cscript.exe
+set HTTPGET_JS="%TEMP%\httpget.js"
+echo (function(b,d){var a=b.Arguments(0);var c=b.Arguments(1);var f=new d("MSXML2.XMLHTTP");f.open("GET",a,false);f.send();if(f.Status==200){var e=new d("ADODB.Stream");e.Open();e.Type=1;e.Write(f.ResponseBody);e.Position=0;e.SaveToFile(c);e.Close()}else{b.Echo("Error: HTTP "+f.status+" "+f.statusText)}})(WScript,ActiveXObject); > %HTTPGET_JS%
+
+if not exist "%~dp0\nodejs" md "%~dp0\nodejs"
+
+pushd "%~dp0\nodejs"
+set zip="%~dp0..\..\tools\7-zip\7z.exe"
+
+if not exist node.exe (
+    echo Downloading node ...
+    cscript //nologo %HTTPGET_JS% http://nodejs.org/dist/latest/node.exe node.exe
+)
+
+if not exist npm.cmd (
+    echo Downloading npm ...
+    if exist npm.zip del npm.zip
+    cscript //nologo %HTTPGET_JS% http://nodejs.org/dist/npm/npm-1.3.9.zip npm.zip
+    echo Extracting npm ...
+    %zip% x npm.zip >nul
+    del npm.zip
+)
+
+if not exist node_modules\coffee-script (
+    call npm install coffee-script --quiet
+)
+
+if not exist node_modules\less (
+    call npm install less --quiet
+)
+
+if not exist ..\Resources\nodejs.tgz (
+    if not exist ..\Resources md ..\Resources
+    
+    if exist %temp%\nodejs.tar del %temp%\nodejs.tar
+
+    %zip% a -r %temp%\nodejs.tar * ^
+        -xr!node_modules\npm\ ^
+        -xr!node_modules\less\test\ ^
+        -xr!node_modules\less\benchmark\ ^
+        -xr!node_modules\less\dist\ ^
+        >nul
+        
+    %zip% a ..\Resources\nodejs.tgz %temp%\nodejs.tar >nul
+	del %temp%\nodejs.tar
+)
+
+popd
