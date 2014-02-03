ef6ec7b0ee279314bb2294a1f0bdcd3ae37ed796
Duncan Smart <duncan.smart@gmail.com>
2014-02-03 11:38:59 +0000

    v2.5.5

--- package/CREATE PACKAGE.cmd
+++ package/CREATE PACKAGE.cmd
@@ -4,5 +4,5 @@ pushd "%~dp0"
 echo Release build LessCoffee.csproj ...
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe ..\src\Handlers\LessCoffee.csproj /nologo /t:Build /p:Configuration=Release /verbosity:quiet
 
-nuget pack LessCoffee.nuspec -version 2.5.2.0 -NoPackageAnalysis
+nuget pack LessCoffee.nuspec -version 2.5.5.0 -NoPackageAnalysis
 
